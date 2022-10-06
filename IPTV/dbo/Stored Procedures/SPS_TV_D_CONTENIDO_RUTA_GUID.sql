93a2fc6278dc351810f11b7ca8e455efc6fd27cb
J Alberto Cruz <acruz@interactivesolutions.mx>
2022-10-06 14:14:24 -0500

    Anexo creacion de SP para busqueda de un campo especifico

---
+++ IPTV/dbo/Stored Procedures/SPS_TV_D_CONTENIDO_RUTA_GUID.sql
@@ -0,0 +1,20 @@
+﻿USE [IPTV]
+GO
+
+-- =============================================
+-- Author:		<Alberto Cruz>
+-- Create date: <06/10/2022>
+-- Description:	<selecciona un campo especifico de la tabla TV_D_CONTENIDO>
+-- =============================================
+
+CREATE PROCEDURE [dbo].[SPS_TV_D_CONTENIDO_RUTA_GUID]
+ @RUTA_GUID VARCHAR(500)
+  AS
+BEGIN
+  SET NOCOUNT ON
+  SELECT NOMBRE
+  FROM [IPTV].[dbo].[TV_D_CONTENIDO]
+  WHERE RUTA like '%'+ @RUTA_GUID +'%'
+  SET NOCOUNT OFF
+END
+GO
